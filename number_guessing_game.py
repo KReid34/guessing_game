ab9b32c2fd3a3356135d684c3e393f1ecd62f205
Karina Reid <72445743+KReid34@users.noreply.github.com>
2020-12-15 21:10:09 +0000

    update_number_guessing_game
    This is a second and shorter way to create the same output!

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -12,43 +12,28 @@ difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
 
 easy_tries_remaining = 10
 hard_tries_remaining = 5
-guess_correct = False
+game_continue = True
 
-while guess_correct == False:
-    if difficulty == 'easy':
-        if easy_tries_remaining > 0:
-            print(f"You have {easy_tries_remaining} tries remaining.")
-            guess = int(input("Make a guess: "))
-            if guess < RANDOM_NUMBER:
-                print("Too low.")
-                print("Guess again.")
-                easy_tries_remaining -= 1
-            elif guess > RANDOM_NUMBER:
-                print("Too high.")
-                print("Guess again.")
-                easy_tries_remaining -= 1
-            elif guess == RANDOM_NUMBER:
-                print(f"You got it! The correct number was {RANDOM_NUMBER}.")
-                guess_correct = True
+while game_continue:
+    guess = int(input("Make a guess: "))
+    print(f"You have {easy_tries_remaining} tries remaining.")
+    if guess < RANDOM_NUMBER:
+        print("Too low.")
+        print("Guess again.")
+        if difficulty == 'easy':
+            easy_tries_remaining -= 1
         else:
-            print(f"You lose. The correct numer is {RANDOM_NUMBER}.")
-            guess_correct = True
-        
-    elif difficulty == 'hard':
-        if hard_tries_remaining > 0:
-            print(f"You have {hard_tries_remaining} tries remaining.")
-            guess = int(input("Make a guess: "))
-            if guess < RANDOM_NUMBER:
-                print("Too low.")
-                print("Guess again.")
-                hard_tries_remaining -= 1
-            elif guess > RANDOM_NUMBER:
-                print("Too high.")
-                print("Guess again.")
-                hard_tries_remaining -= 1
-            elif guess == RANDOM_NUMBER:
-                print(f"You got it! The correct number was {RANDOM_NUMBER}.")
-                guess_correct = True
+            hard_tries_remaining -= 1
+    elif guess > RANDOM_NUMBER:
+        print("Too high.")
+        print("Guess again.")
+        if difficulty == 'easy':
+            easy_tries_remaining -= 1
         else:
-            print(f"You lose. The correct numer is {RANDOM_NUMBER}.")
-            guess_correct = True
+            hard_tries_remaining -= 1
+    elif guess == RANDOM_NUMBER:
+        print(f"You got it! The correct number was {RANDOM_NUMBER}.")
+        game_continue = False
+    else:
+        print(f"You lose. The correct numer is {RANDOM_NUMBER}.")
+        game_continue = False
